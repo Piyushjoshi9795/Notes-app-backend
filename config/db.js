f686077607e0fa393421150e0cacfae44169a7d1
PIYUSH JOSHI <piyushjoshi9795@gmail.com>
2026-04-30 12:08:09 +0530

    updating

---
+++ config/db.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose');
+
+const connectMongo=async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("mngodb connected successfilly");
+    }
+    catch(err){
+        console.log("Error in connecting to mongodb",err);
+    }
+}
+
+module.exports=connectMongo;
